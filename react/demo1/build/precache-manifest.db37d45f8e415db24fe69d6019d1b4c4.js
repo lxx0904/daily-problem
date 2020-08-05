00495194119878efa81644fb1aff47f61215e889
lanxx <lanxx@halanqian.com>
2020-08-05 09:57:08 +0800

    Signed-off-by: lanxx <lanxx@halanqian.com>

---
+++ react/demo1/build/precache-manifest.db37d45f8e415db24fe69d6019d1b4c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db40603e66651ea06f6293eb1d04636a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "69e71262f2ee9e39e496",
+    "url": "./static/css/2.9c8c8187.chunk.css"
+  },
+  {
+    "revision": "2a9b747f8da7fca81446",
+    "url": "./static/css/main.3d1608ae.chunk.css"
+  },
+  {
+    "revision": "69e71262f2ee9e39e496",
+    "url": "./static/js/2.c75176b6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.c75176b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a9b747f8da7fca81446",
+    "url": "./static/js/main.eb2c0575.chunk.js"
+  },
+  {
+    "revision": "305669174611681563c9",
+    "url": "./static/js/runtime-main.fe10b6f2.js"
+  }
+]);
